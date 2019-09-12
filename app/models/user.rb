3a40931b09ffb4b7c86fea764df4631e3d1e258f
doandainghia <doan.dai.nghia.123@gmail.com>
2019-09-12 11:41:07 +0700

    Add more validate to model movie

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   has_many :users
 
-  validates :name,  presence: true, length: { maximum: 50 }
+  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
