5a9d6ca0938b47af87a480646ffb73bc555c73db
doandainghia <doan.dai.nghia.123@gmail.com>
2019-09-12 09:02:31 +0700

    Add url validation of movie

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,6 +1,6 @@
 module MoviesHelper
   def display_video(url)
-    url_id = YouTubeRails.extract_video_id(url)
+    url_id = YouTubeRails.extract_video_id(url) || url
     "https://www.youtube.com/embed/#{url_id}"
   end
 end
