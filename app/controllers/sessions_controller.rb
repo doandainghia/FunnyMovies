ec8e5f37ee564f7f497735c30f9fac67cb5b13f7
doandainghia <doan.dai.nghia.123@gmail.com>
2019-09-12 09:49:51 +0700

    Add class to button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
+      redirect_to root_path
     else
       flash[:danger] = 'Invalid email/password combination'
       redirect_to root_path
